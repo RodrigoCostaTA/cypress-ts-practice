3863ccd8511ce828d93762668bcf4e828c71fee5
RodrigoCostaTA <rcosta@solvd.com>
2023-12-26 17:03:53 -0300

    Requested changes

--- cypress/e2e/product-details.cy.ts
+++ cypress/e2e/product-details.cy.ts
@@ -1,42 +1,43 @@
 describe('Product Details Page Tests', () => {
-    const modelName = 'Apple MacBook';
 
     beforeEach(() => {
         cy.visit('https://demo.nopcommerce.com/');
-
-        cy.get('#small-searchterms').type(modelName);
         
-        cy.get('.search-box-button').click();  
+        cy.fixture('productData.json').then((data) => {
+            const notebook = data.notebook;
+            cy.get('#small-searchterms').type(notebook);
+            cy.get('.search-box-button').click();  
 
-        cy.contains(modelName).click();
+            cy.contains(notebook).click();
+        });
     });
 
     it('should validate the Product Details Page', () => {
+        cy.fixture('productData.json').then((data) => {
+            const notebook = data.notebook;
 
-        // Verify the title of the Product Details Page
-        cy.title().should('include', modelName);
+            // Verify the title of the Product Details Page
+            cy.title().should('include', notebook);
 
-        // Verify product overview
-        cy.get('.short-description').should('exist');
-        cy.get('.manufacturers').should('exist');
-        cy.get('.sku').should('exist');
-        cy.get('#price-value-4').should('exist');
+            // Verify product overview
+            cy.get('.short-description').should('exist');
+            cy.get('.manufacturers').should('exist');
+            cy.get('.sku').should('exist');
+            cy.get('#price-value-4').should('exist');
 
-        // Verify comprehensive specifications
-        cy.contains('Products specifications').should('exist');
-        cy.contains('Screensize');
-        cy.contains('CPU Type');
-        cy.contains('Memory');
+            // Verify comprehensive specifications
+            cy.get('.product-specs-box').contains('Products specifications').should('exist');
+            cy.get('.spec-name').contains('Screensize').should('exist');
+            cy.get('.spec-name').contains('CPU Type').should('exist');
+            cy.get('.spec-name').contains('Memory').should('exist');
 
-        // Verify reviews section
-        cy.get('.product-review-links').should('exist');
-        cy.get('.product-review-box').should('exist');
+            // Verify reviews section
+            cy.get('.product-review-links').should('exist');
+            cy.get('.product-review-box').should('exist');
 
-        cy.get('.picture-thumbs').click();
-        cy.get('#main-product-img-4').should('be.visible');
-
-        cy.viewport(320, 480); // Mobile view
-        cy.viewport(1280, 720); // Desktop view
+            cy.get('.picture-thumbs').click();
+            cy.get('#main-product-img-4').should('be.visible');
+        });
     });
     
 });
