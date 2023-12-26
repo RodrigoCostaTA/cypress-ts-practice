4ac07326bab6a5bf231d61c616adc01239d7546f
RodrigoCostaTA <rcosta@solvd.com>
2023-12-26 18:25:42 -0300

    Update wishlist-functionality.cy.ts
    Refactored flaky test

--- cypress/e2e/wishlist-functionality.cy.ts
+++ cypress/e2e/wishlist-functionality.cy.ts
@@ -7,20 +7,20 @@ describe('Verify Wishlist Functionality', () => {
       cy.visit('https://demo.nopcommerce.com/apple-macbook-pro-13-inch');
       
       // Add the product to the wishlist
+      cy.get('.wishlist-label').should('be.visible');
+      cy.get('.wishlist-qty').should('be.visible');
       cy.get('#add-to-wishlist-button-4').click();
-      cy.get('.bar-notification').should('be.visible');
       
-      // Navigate to the Wishlist page
-      cy.get('.ico-wishlist').click(); // Replace with the actual URL
+      //Go to the wishlist page
+      cy.get('.wishlist-label').click();
       
       // Verify that the product is added to the wishlist
-      cy.contains(modelName).should('exist'); // Replace 'Product Name' with the actual product name
+      cy.contains(modelName).should('exist'); 
       
       // Manage wishlist items
-      
       // Verify product removal
       cy.get('.remove-btn').click();
-      cy.contains(modelName).should('not.exist'); // Verify product removal
+      cy.contains(modelName).should('not.exist');
     });
         
   });
