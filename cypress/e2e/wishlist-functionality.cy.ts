c55515d92ee26b1a5a10277ac0bec128166551ad
RodrigoCostaTA <rcosta@solvd.com>
2023-12-27 12:09:06 -0300

    Requested changes
    Added cypress-wait-until dependency

--- cypress/e2e/wishlist-functionality.cy.ts
+++ cypress/e2e/wishlist-functionality.cy.ts
@@ -1,26 +1,35 @@
-describe('Verify Wishlist Functionality', () => {
-    const modelName = 'Apple MacBook';
+import 'cypress-wait-until';
 
-    
-    it('should add a product to the wishlist and verify its presence', () => {
-      // Navigate to the product details page
-      cy.visit('https://demo.nopcommerce.com/apple-macbook-pro-13-inch');
-      
-      // Add the product to the wishlist
-      cy.get('.wishlist-label').should('be.visible');
-      cy.get('.wishlist-qty').should('be.visible');
-      cy.get('#add-to-wishlist-button-4').click();
-      
-      //Go to the wishlist page
-      cy.get('.wishlist-label').click();
-      
-      // Verify that the product is added to the wishlist
-      cy.contains(modelName).should('exist'); 
-      
-      // Manage wishlist items
-      // Verify product removal
-      cy.get('.remove-btn').click();
-      cy.contains(modelName).should('not.exist');
-    });
-        
+describe('Verify Wishlist Functionality', () => {
+  const modelName = 'Apple MacBook';
+
+  it('should add a product to the wishlist and verify its presence', () => {
+    // Navigate to the product details page
+    cy.visit('https://demo.nopcommerce.com/apple-macbook-pro-13-inch');
+
+    // Wait until the add to wishlist button is visible
+    cy.waitUntil(() => 
+      cy.get('#add-to-wishlist-button-4')
+        .should('be.visible')
+        .and('be.enabled')
+    );
+
+    // Add the product to the wishlist
+    cy.get('#add-to-wishlist-button-4').click();
+
+    // Navigate to the wishlist page
+    cy.get('.wishlist-label').click();
+
+    // Verify that the product is added to the wishlist
+    cy.get('.product-name').contains(modelName).should('be.visible');
+
+    // Manage wishlist items
+    // Remove the product from the wishlist
+    cy.waitUntil(() =>
+      cy.get('.remove-btn').should('be.visible').click()
+    );
+    cy.get('.remove-btn').click()
+    // Verify product removal
+    cy.contains(modelName).should('not.exist');
   });
+});
