d0edb97356626a0b27b8c88983d1177403a67d3a
RodrigoCostaTA <rcosta@solvd.com>
2023-12-28 12:05:23 -0300

    Update wishlist-functionality.cy.ts

--- cypress/e2e/wishlist-functionality.cy.ts
+++ cypress/e2e/wishlist-functionality.cy.ts
@@ -8,14 +8,14 @@ describe('Verify Wishlist Functionality', () => {
     cy.visit('https://demo.nopcommerce.com/apple-macbook-pro-13-inch');
 
     // Wait until the add to wishlist button is visible
-    cy.waitUntil(() => 
-      cy.get('#add-to-wishlist-button-4')
-        .should('be.visible')
-        .and('be.enabled')
-    );
+    cy.wait(500)
 
     // Add the product to the wishlist
     cy.get('#add-to-wishlist-button-4').click();
+
+    cy.waitUntil( () => cy.get(".bar-notification.success").then( (success)=> {
+      return success.is(":visible")
+    }), {})
 
     // Navigate to the wishlist page
     cy.get('.wishlist-label').click();
@@ -23,12 +23,15 @@ describe('Verify Wishlist Functionality', () => {
     // Verify that the product is added to the wishlist
     cy.get('.product-name').contains(modelName).should('be.visible');
 
+    
     // Manage wishlist items
+
+    cy.waitUntil( () => cy.get(".remove-btn").then( (success)=> {
+      return success.is(":visible")
+    }), {})
     // Remove the product from the wishlist
-    cy.waitUntil(() =>
-      cy.get('.remove-btn').should('be.visible').click()
-    );
     cy.get('.remove-btn').click()
+    
     // Verify product removal
     cy.contains(modelName).should('not.exist');
   });
